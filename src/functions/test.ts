e880d74b31527acc39a68c52c4c5d4461a969638
MiroslavPeterka1 <miroslav.peterka1@gmail.com>
2025-10-28 12:49:40 +0100

    feat: integrate NestJS into Azure Functions
    - Added NestJS core and app module dependencies to package.json.
    - Updated TypeScript version and added @types/jest for testing.
    - Refactored test function to utilize NestJS AppService for response generation.
    - Updated tsconfig.json to target ES2022 and enable experimental decorators.

--- src/functions/test.ts
+++ src/functions/test.ts
@@ -1,15 +1,21 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../../nest-handler/src/app.module';
+import { AppService } from '../../nest-handler/src/app.service';
 
 export async function test(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+  context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+  // const name = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}!` };
-};
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const appService = appContext.get(AppService);
+  const name = appService.getHello();
+  return { body: `Hello, ${name}!` };
+}
 
 app.http('test', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: test
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: test,
 });
